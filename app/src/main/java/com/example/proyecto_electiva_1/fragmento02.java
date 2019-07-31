a0297d10f957d680284ec47add10f9de86ba6945
FelipeCristancho8 <38051232+FelipeCristancho8@users.noreply.github.com>
2019-07-31 17:39:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/proyecto_electiva_1/fragmento02.java
@@ -0,0 +1,29 @@
+package com.example.proyecto_electiva_1;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link fragmento02.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link fragmento02#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class fragmento02 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragmento02, container, false);
+    }
+}
